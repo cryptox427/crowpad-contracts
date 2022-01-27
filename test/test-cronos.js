b0c37914de5c61a26edbb23c3ef103329f18124f
smith <smithzhang0427@gmail.com>
2022-01-27 15:58:04 -0500

    Deposite & Refund on CrowpadSale

--- test/test-cronos.js
+++ test/test-cronos.js
@@ -189,6 +189,7 @@ describe("CrowpadSaleFactory", function () {
       "10",
       "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
       "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+      "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
       9999,
       12312312,
       15555555,
